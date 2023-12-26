383391fb301b3450926776e46a15a5705ec88a92
MiroKaku <50670906+MiroKaku@users.noreply.github.com>
2023-12-26 18:26:26 +0800

    build: don't change directory.

--- BuildAllTargets.cmd
+++ BuildAllTargets.cmd
@@ -1,16 +1,18 @@
-@setlocal
 @echo off
 
-rem Change to the current folder.
-cd "%~dp0"
+setlocal
+pushd "%~dp0"
 
 rem Remove the output folder for a fresh compile.
 rd /s /q Output
 
 rem Initialize Visual Studio environment
-call "%~dp0Mile.Project.Windows\InitializeVisualStudioEnvironment.cmd"
+if "%VSINSTALLDIR%"=="" (
+    call "Mile.Project.Windows\InitializeVisualStudioEnvironment.cmd"
+)
 
 rem Build all targets
 MSBuild -binaryLogger:Output\BuildAllTargets.binlog -m BuildAllTargets.proj
 
-@endlocal
+popd
+endlocal
